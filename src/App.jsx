701a92e5d458d59211c07237fb7406f4c0f8112e
Jarzembak <63061293+Jarzembak@users.noreply.github.com>
2024-02-24 21:03:55 -0500

    Rendered featured puppy details

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-
-  console.log(puppies);
+  const featuredPup = puppies.find((puppy) => featPupId === puppy.id)
+  console.log(featuredPup);
 
   return (
     <>
@@ -16,7 +16,16 @@ function App() {
           return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
-      {featPupId && <p>{featPupId}</p>}
+      {featPupId && (
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+      )}
+      
     </>
   )
 }
